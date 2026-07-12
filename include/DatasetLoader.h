863753b6d6fe05d4b0a1b058c3aa0090d44b185d
muratatar06 <murattalhaatar@gmail.com>
2026-07-11 22:56:51 -0400

    Fix dataset loader header and includes

--- include/DatasetLoader.h
+++ include/DatasetLoader.h
@@ -1,68 +1,16 @@
-#include "DatasetLoader.h"
+#ifndef DATASET_LOADER_H
+#define DATASET_LOADER_H
 
-#include <fstream>
-#include <iostream>
-#include <sstream>
+#include "Movie.h"
 
-std::vector<Movie> DatasetLoader::loadMovies(
-    const std::string& basicsFile
-) {
-    std::vector<Movie> movies;
+#include <string>
+#include <vector>
 
-    std::ifstream basicsStream(basicsFile);
+class DatasetLoader {
+public:
+    static std::vector<Movie> loadMovies(
+        const std::string& basicsFile
+    );
+};
 
-    if (!basicsStream.is_open()) {
-        std::cerr << "Could not open basics file: "
-                  << basicsFile << '\n';
-        return movies;
-    }
-
-    std::string line;
-
-    std::getline(basicsStream, line);
-
-    while (std::getline(basicsStream, line)) {
-        std::stringstream stream(line);
-
-        std::string id;
-        std::string titleType;
-        std::string primaryTitle;
-        std::string originalTitle;
-        std::string isAdult;
-        std::string startYear;
-        std::string endYear;
-        std::string runtimeMinutes;
-        std::string genres;
-
-        std::getline(stream, id, '\t');
-        std::getline(stream, titleType, '\t');
-        std::getline(stream, primaryTitle, '\t');
-        std::getline(stream, originalTitle, '\t');
-        std::getline(stream, isAdult, '\t');
-        std::getline(stream, startYear, '\t');
-        std::getline(stream, endYear, '\t');
-        std::getline(stream, runtimeMinutes, '\t');
-        std::getline(stream, genres, '\t');
-
-        if (titleType != "movie" || startYear == "\\N") {
-            continue;
-        }
-
-        int year;
-
-        try {
-            year = std::stoi(startYear);
-        } catch (...) {
-            continue;
-        }
-
-        movies.push_back({
-            id,
-            primaryTitle,
-            year,
-            0.0f
-        });
-    }
-
-    return movies;
-}
+#endif
